721996697fa5763817361057f44000837db22b31
afroj07 <afrojkhanafroj70@gmail.com>
2023-09-07 14:34:26 +0530

    Add form, footer and firebase

--- src/componets/Contact.jsx
+++ src/componets/Contact.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import img from '../assets/msg.jpg';
 
 const Contact = () => {
+
+   const [name,setName]=useState('');
+   const [email,setEmail]=useState('');
+   const [message,setMeassage]=useState('');
+   const submitHandler=(e)=>{
+      e.preventDefault();
+        console.log(name, email, message);
+   }
   return (
     <div id='contact'>
  <section>
-    <form >
+    <form onSubmit={submitHandler}>
         <h2>Contact Me</h2>
-        <input type="text" placeholder='Your Name ' required/>
-        <input type="email" placeholder='example@gmail.com' required />
+        <input type="text"
+        value={name}
+        onChange={(e)=>setName(e.target.value)}
+         placeholder='Your Name ' required/>
+        <input type="email"
+         value={email}
+         onChange={(e)=>setEmail(e.target.value)}
+        placeholder='example@gmail.com' required />
        
-        <input type="text" placeholder='Start message'required />
+        <input type="text" 
+         value={message}
+         onChange={(e)=>setMeassage(e.target.value)}
+        placeholder='Start message'required />
         <button type='submit'>Send</button>
                  
     </form>
