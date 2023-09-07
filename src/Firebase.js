7cf66245b04e7e3ff3ca2d975c425062d9fe442a
afroj07 <afrojkhanafroj70@gmail.com>
2023-09-07 23:18:08 +0530

    MEdiaQuery

---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+import {getFirestore} from  'firebase/firestore'
+const firebaseConfig = {
+  apiKey: "AIzaSyBhi12hHDxmaB9ly2RV6WsEu4IJ37EuyRU",
+  authDomain: "afroj-portfolio.firebaseapp.com",
+  projectId: "afroj-portfolio",
+  storageBucket: "afroj-portfolio.appspot.com",
+  messagingSenderId: "397800027395",
+  appId: "1:397800027395:web:d838b4a2d23a3661fd6536"
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
+
+export const db =getFirestore
--- src/componets/Contact.jsx
+++ src/componets/Contact.jsx
@@ -1,22 +1,71 @@
 import React, { useState } from 'react'
 import img from '../assets/msg.jpg';
 import  toast from 'react-hot-toast';
-import
-
+import { motion } from 'framer-motion';
+import {addDoc,collection}from 'firebase/firestore'
+import {db} from '../Firebase'
 const Contact = () => {
 
    const [name,setName]=useState('');
    const [email,setEmail]=useState('');
    const [message,setMeassage]=useState('');
-   const submitHandler=(e)=>{
+
+   const [disableBtn, setDisableBtn]= useState(false);
+
+   const submitHandler=async(e)=>{
       e.preventDefault();
-        console.log(name, email, message);
-        toast.success('Meassage sent successfully');
+      setDisableBtn(true);
+       
+        try {
+         
+         await addDoc(collection(db,'contacts'), 
+         {
+          name, 
+          email , 
+          message,
+       });
+       toast.success('Meassage sent successfully');
+       setDisableBtn(false);
+        } catch (error) {
+         toast.error('Error');
+         console.log(error);
+         setDisableBtn(f)
+   
+        }
    }
+
+const animations={
+   form:{
+      initial:{
+         x:"-100%",
+         opacity:0,
+
+      },
+      whileInView:{
+         x:0,
+         opacity:1,
+      },
+   },
+   button:{
+      initial:{
+         y:"-100%",
+         opacity:0,
+
+      },
+      whileInView:{
+         y:0,
+         opacity:1,
+      },
+      transition:{
+         delay:0.5
+      }
+   },
+}
+
   return (
     <div id='contact'>
  <section>
-    <form onSubmit={submitHandler}>
+    <motion.form onSubmit={submitHandler}{...animations.form}>
         <h2>Contact Me</h2>
         <input type="text"
         value={name}
@@ -31,9 +80,11 @@ const Contact = () => {
          value={message}
          onChange={(e)=>setMeassage(e.target.value)}
         placeholder='Start message'required />
-        <button type='submit'>Send</button>
+        <motion.button  disabled={disableBtn} 
+        className={disableBtn?"disabledBtn":""}
+        {...animations.button} type='submit'>Send</motion.button>
                  
-    </form>
+    </motion.form>
  </section>
  <aside>
     <img src={img} alt="sideImage" />
